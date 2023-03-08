1ec086fd56e18c255f7701555e35bb5b692a2aee
Javier Pena <jpena@redhat.com>
2023-03-08 16:24:34 +0100

    Fix ptp-config.sh script
    It was missing the Logan Beach support and loop already included in [1].
    Also add a small pause before enabling the SMA port.
    
    [1] - https://github.com/RHsyseng/oot-ice/commit/7713208be7e108c05d0ef3692dca5f4d36aa1246

--- ptp-config.sh
+++ ptp-config.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 set -eu
 
-ETH=$(grep 000e /sys/class/net/*/device/subsystem_device | awk -F"/" '{print $5}' | head -n 1)
+ETH=$(grep -e 000e -e 000f /sys/class/net/*/device/subsystem_device | awk -F"/" '{print $5}')
 
-echo 0 2 > /sys/class/net/$ETH/device/ptp/ptp*/pins/U.FL2
-echo 0 1 > /sys/class/net/$ETH/device/ptp/ptp*/pins/U.FL1
-echo 0 2 > /sys/class/net/$ETH/device/ptp/ptp*/pins/SMA2
-echo 0 1 > /sys/class/net/$ETH/device/ptp/ptp*/pins/SMA1
+for DEV in $ETH; do
+  if [ -f /sys/class/net/$DEV/device/ptp/ptp*/pins/U.FL2 ]; then
+    echo 0 2 > /sys/class/net/$DEV/device/ptp/ptp*/pins/U.FL2
+    echo 0 1 > /sys/class/net/$DEV/device/ptp/ptp*/pins/U.FL1
+    echo 0 2 > /sys/class/net/$DEV/device/ptp/ptp*/pins/SMA2
+    echo 0 1 > /sys/class/net/$DEV/device/ptp/ptp*/pins/SMA1
+  fi
+done
 
 echo "Disabled all SMA and U.FL Connections"
+
+sleep 3
 
 # Set the following variables only if you want to have a dual-NIC setup
 # where one of the NICs receives the GPS signal via GNSS, and transfers
